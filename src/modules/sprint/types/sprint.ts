f072f6ab74c9d69dffb64e0c7e0dd653df424418
NishanthRMS <Nishanth.R@mitrahsoft.io>
2026-07-23 16:26:04 +0530

    feat: sprint ui

---
+++ src/modules/sprint/types/sprint.ts
@@ -0,0 +1,37 @@
+import { Priority } from '@/src/types/board';
+
+export interface SprintStat {
+  value: number | string;
+  title: string;
+  valueColor: string;
+}
+
+export interface ProgressCardData {
+  title: string;
+  progress: number;
+  progressColor: string;
+  rightLabel: string;
+  subtitle?: string;
+  startDate?: string;
+  endDate?: string;
+}
+
+export interface Task {
+  title: string;
+  priority: Priority;
+}
+
+export interface TaskColumnData {
+  status: string;
+  count: number;
+  dotColor: string;
+  task?: Task;
+}
+
+export interface Workload {
+  initials: string;
+  name: string;
+  color: string;
+  completed: number;
+  total: number;
+}
